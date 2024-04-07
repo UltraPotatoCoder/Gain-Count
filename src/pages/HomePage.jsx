baca1c804775fd0aa1501a336ed6de1a3340e6fc
Guilherme Carvalho <guicarv99@gmail.com>
2024-04-07 16:41:01 +0100

    Home page basic structure made

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,20 @@
+import '../components/CSS/HomePage.css';
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div className='welcome'>
+        <h3>Welcome To Gain Count Homie</h3>
+
+        <div>
+          <div className='addworkout'>
+            <button> Add a Workout +</button>
+            <img src='/src/assets/arnold.gif' alt='arnold' />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
